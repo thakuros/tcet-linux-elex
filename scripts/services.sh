e27cc4b774c68fe522773b792f21d946f9615440
demonkiller <subhadravishwakarma8gris@gmail.com>
2021-05-10 00:19:01 +0530

    updated servies script with licence

--- scripts/services.sh
+++ scripts/services.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+#set -e
+#=================================================================================
+#Author: DemonKiller
+#SPDX-License-Identifier: GPL-3.0
+#Warning! DO NOT RUN THIS SCRIPT BLINDLY. MAKE CHANGES ACCORDING TO YOUR WORKFLOW.
+#RUN AT YOUR OWN RISK.
+#=================================================================================
+
 cd ..
 cd makeiso/airootfs/etc/systemd/system/
 tput setaf 2
