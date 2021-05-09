016d5afb8e41e9e035d831ff2cb63f14c50d5cf8
demonkiller <subhadravishwakarma8gris@gmail.com>
2021-05-09 21:15:12 +0530

    updated makeiso and packages

---
+++ scripts/cleanup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#!/bin/bash
+#set -e
+#=================================================================================
+#Author: DemonKiller
+#SPDX-License-Identifier: GPL-3.0
+#=================================================================================
+#set -e
+
+sudo rm -rvf out/ 
+sudo rm -rvf work/
+
+tput setaf 2
+echo "D O N E!"
+tput srg0
